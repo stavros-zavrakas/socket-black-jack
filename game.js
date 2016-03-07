ec52103b485489057853e82ab9dd6a784a075480
Stavros Zavrakas <szavrakas@cambridge.org>
2016-03-07 23:19:49 +0000

    class prototype style

--- game.js
+++ game.js
@@ -4,7 +4,7 @@
 // 27-39 = 1-13 Clubs
 // 40-52 = 1-13 Spades
 
-function Game (values) {
+function Game(values) {
   if (values === null || typeof values === 'undefined') {
     values = {};
   }
@@ -13,14 +13,14 @@ function Game (values) {
   this.players_size = 0;
   this.turn = false;
   this.turnsList = [];
-  this.deck = Array.apply(null, Array(52)).map(function (x, i) { 
-    return i+1 
+  this.deck = Array.apply(null, Array(52)).map(function (x, i) {
+    return i + 1
   });
 
-  var started = values.started || false;
-  var max_players = values.max_players || 1;
-  
-  var players = {
+  this.started = values.started || false;
+  this.max_players = values.max_players || 1;
+
+  this.players = {
     names: [],
     dealer: {
       name: 'dealer',
@@ -30,51 +30,45 @@ function Game (values) {
     }
   };
 
-  this.initPlayer = function (player_name, socket) {
-    // @todo: maybe checking the indexOf players.names array?
-    if (typeof players[player_name] !== 'undefined') {
-      return false;
-    }
-    
-    players.names.push(player_name);
-    players[player_name] = {
-      name: player_name,
-      socket: socket,
-      cards: [],
-      sum: 0
-    };
+};
 
-    return true;
+Game.prototype.initPlayer = function (player_name, socket) {
+  // @todo: maybe checking the indexOf players.names array?
+  if (typeof this.players[player_name] !== 'undefined') {
+    return false;
+  }
+
+  this.players.names.push(player_name);
+  this.players[player_name] = {
+    name: player_name,
+    socket: socket,
+    cards: [],
+    sum: 0
   };
 
-  this.updatePlayerCards = function (player_name, card) {
-    // @todo: check if the players_name exists in the object
-    players[player_name].cards.push(card);
-  };
-
-  this.getPlayers = function () {
-    return players;
-  };
-
-  this.getPlayerNames = function () {
-    return players.names;
-  };
-
-  this.getStarted = function() {
-    return started;
-  };
-
-  this.setStarted = function(started_over) {
-    started = started_over;
-  };
-
-  this.getMaxPlayers = function() {
-    return max_players;
-  };
-
-  this.setMaxPlayers = function(max_players_over) {
-    max_players = max_players_over;
-  };
+  return true;
+};
+Game.prototype.updatePlayerCards = function (player_name, card) {
+  // @todo: check if the players_name exists in the object
+  this.players[player_name].cards.push(card);
+};
+Game.prototype.getPlayers = function () {
+  return this.players;
+};
+Game.prototype.getPlayerNames = function () {
+  return this.players.names;
+};
+Game.prototype.getStarted = function () {
+  return this.started;
+};
+Game.prototype.setStarted = function (started_over) {
+  this.started = started_over;
+};
+Game.prototype.getMaxPlayers = function () {
+  return this.max_players;
+};
+Game.prototype.setMaxPlayers = function (max_players_over) {
+  this.max_players = max_players_over;
 };
 
 module.exports = Game;
